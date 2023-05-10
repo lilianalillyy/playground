17538faca730a72fa2be6e8a850d89194bf4e9ea
Mia Vališová (Liliana) <118116415+lilianalillyy@users.noreply.github.com>
2023-05-10 09:58:05 +0200

    chore: update fixtriathlon

--- fixtriathlon/fixmouse.sh
+++ fixtriathlon/fixmouse.sh
@@ -1,4 +1,7 @@
-ID=$(xinput --list | grep M720.*Keyboard | sed -E 's/.*id=([0-9]+).*/\1/g');
+## One of the following ID variables should work
+#ID=$(xinput --list | grep M720.*Keyboard | sed -E 's/.*id=([0-9]+).*/\1/g');
+#ID=$(xinput list --id-only "keyboard:Logitech M720 Triathlon")
+ID=$(xinput list --id-only "keyboard:M720 Triathlon Keyboard")
 OUT=$(xinput disable $ID 2>&1);
 LOG="/home/liliana/fixmouse.log";
 
@@ -8,6 +11,3 @@ if [ -n "${LOG}" ]; then
     echo $OUT >> $LOG;
     echo "" >> fixmouse.log;
 fi
-
-
-# xinput disable $(xinput --list | grep M720.*keyboard | sed -E 's/.*id=([0-9]+).*/\1/g')
